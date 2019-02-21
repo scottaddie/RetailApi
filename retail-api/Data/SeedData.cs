38007e1b69cbed39e19b74f61ac013ff29dc7143
Scott Addie <scott.addie@microsoft.com>
2019-02-21 11:25:03 -0600

    Cleanup of SeedData class

--- retail-api/Data/SeedData.cs
+++ retail-api/Data/SeedData.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using Microsoft.EntityFrameworkCore;
 using RetailApi.Models;
 
 namespace RetailApi.Data
@@ -11,38 +12,72 @@ namespace RetailApi.Data
             if (!context.Products.Any())
             {
                 context.Products.AddRange(
-                    new Product { Name = "Squeaky Bone", Price = 20.99m },
-                    new Product { Name = "Knotted Rope", Price = 12.99m }
+                    new Product
+                    {
+                        Name = "Squeaky Bone",
+                        Price = 20.99m
+                    },
+                    new Product
+                    {
+                        Name = "Knotted Rope",
+                        Price = 12.99m
+                    }
                 );
+
                 context.SaveChanges();
             }
 
             if (!context.Customers.Any())
             {
                 context.Customers.AddRange(
-                    new Customer { FirstName = "Scott", LastName = "Addie", StreetAddress = "", City = "", StateOrProvinceAbbr = "", Country = "", PostalCode = "", Phone = "", Email = "" },
-                    new Customer { FirstName = "Cam", LastName = "Soper", StreetAddress = "", City = "", StateOrProvinceAbbr = "", Country = "", PostalCode = "", Phone = "", Email = "" }
+                    new Customer
+                    {
+                        FirstName = "Scott",
+                        LastName = "Addie",
+                        StreetAddress = "",
+                        City = "",
+                        StateOrProvinceAbbr = "",
+                        Country = "",
+                        PostalCode = "",
+                        Phone = "",
+                        Email = ""
+                    },
+                    new Customer
+                    {
+                        FirstName = "Cam",
+                        LastName = "Soper",
+                        StreetAddress = "",
+                        City = "",
+                        StateOrProvinceAbbr = "",
+                        Country = "",
+                        PostalCode = "",
+                        Phone = "",
+                        Email = ""
+                    }
                 );
+
                 context.SaveChanges();
             }
 
             if (!context.Orders.Any())
             {
-                var customer1 = context.Customers.OrderBy(c => c.Id).FirstOrDefault();
-                var customer2 = context.Customers.OrderBy(c => c.Id).LastOrDefault();
+                IQueryable<Customer> sortedCustomers =
+                    from c in context.Customers.AsNoTracking()
+                    orderby c.Id
+                    select c;
 
                 context.Orders.AddRange(
                     new Order
                     {
                         OrderPlaced = DateTime.UtcNow.AddDays(-1),
                         OrderFulfilled = DateTime.UtcNow.AddHours(1),
-                        Customer = customer1
+                        Customer = sortedCustomers.FirstOrDefault()
                     },
                     new Order
                     {
                         OrderPlaced = DateTime.UtcNow.AddDays(-3),
                         OrderFulfilled = DateTime.UtcNow.AddHours(4),
-                        Customer = customer2
+                        Customer = sortedCustomers.LastOrDefault()
                     }
                 );
 
@@ -51,9 +86,28 @@ namespace RetailApi.Data
 
             if (!context.ProductOrder.Any())
             {
+                IQueryable<Order> sortedOrders =
+                    from o in context.Orders.AsNoTracking()
+                    orderby o.Id
+                    select o;
+                IQueryable<Product> sortedProducts =
+                    from p in context.Products.AsNoTracking()
+                    orderby p.Id
+                    select p;
+
                 context.ProductOrder.AddRange(
-                    new ProductOrder { Order = context.Orders.OrderBy(o => o.Id).FirstOrDefault(), Product = context.Products.OrderBy(p => p.Id).First(), Quantity = 10 },
-                    new ProductOrder { Order = context.Orders.OrderBy(o => o.Id).LastOrDefault(), Product = context.Products.OrderBy(p => p.Id).Last(), Quantity = 2 }
+                    new ProductOrder
+                    {
+                        Order = sortedOrders.FirstOrDefault(),
+                        Product = sortedProducts.FirstOrDefault(),
+                        Quantity = 10
+                    },
+                    new ProductOrder
+                    {
+                        Order = sortedOrders.LastOrDefault(),
+                        Product = sortedProducts.LastOrDefault(),
+                        Quantity = 2
+                    }
                 );
 
                 context.SaveChanges();
