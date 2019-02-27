b53d3254afd2d6b3c36fe5b0f5b1eb6250b63f4f
Scott Addie <scott.addie@microsoft.com>
2019-02-27 16:22:16 -0600

    Remove unused using statement

--- retail-api/Controllers/OrdersController.cs
+++ retail-api/Controllers/OrdersController.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using RetailApi.DataTransferObjects;
 using RetailApi.Models;
